c4d0570971a414276c264938a9e7a92f78ac9353
Nguyenthile <leevt1997@gmail.com>
2019-01-11 00:11:50 +0700

    fix html, css

--- js/bai1/JS.js
+++ js/bai1/JS.js
@@ -14,15 +14,17 @@ function add_child(){
 	parent.setAttribute("class", "parent");
 	
 	var list_child = document.getElementById("list_child");
-	
+
 	var child = list_child.children;
 	
-	/*for(var i=0; i<=child.length;i++)
+	for(var i=0; i < child.length; i++)
 	{
 		if(child[i].children[1].innerHTML==name)
+		{
 			alert("Product name is not available");
-		return;
-	}*/
+			return;
+		}
+	}
 
 	var index = document.createElement("div");
 	index.setAttribute("class","border");
@@ -43,6 +45,21 @@ function add_child(){
 	parent.appendChild(index);
 	parent.appendChild(product_name);
 	parent.appendChild(img);
-	list_child.appendChild(parent)
+	list_child.appendChild(parent);
+
+	//create event for buttom delete product
+
+	img_delete.onclick = function()
+	{
+		var deleted = close.parentNode;
+        var parentDelete = deleted.parentNode;
+        parentDelete.removeChild(deleted);
+        var dir = deleted.children[0].innerHTML;
+
+        //set index
+        for (i = dir - 1; i < parentDelete.children.length; i++) {
+            parentDelete.children[i].children[0].innerHTML = i + 1;
+        }
+	}
 				
 }
