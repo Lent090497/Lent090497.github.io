793a0998c0efd8753fcb7f888de0fc28cf1b2168
Nguyenthile <leevt1997@gmail.com>
2019-01-13 22:24:02 +0700

    fixed bai 2 js

---
+++ js/bai2/image/JS.js
@@ -0,0 +1,5 @@
+function(){
+	var day=['Sun','Mon','Tue','Web','Thu','Fri','Sat'];
+	var month_name=['January','February','March','April','May','June','July','August','September','October','November','December'];
+	
+}
