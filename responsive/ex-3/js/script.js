7b95b79fa2ce0fd783c1a129ade913cde169a120
Nguyenthile <leevt1997@gmail.com>
2019-02-12 18:05:30 +0700

    fix bai 3

--- responsive/ex-3/js/script.js
+++ responsive/ex-3/js/script.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
-	$('.header-menu1 ul li').hover(function() {
-		/* Stuff to do when the mouse enters the element */
-		var locate=$('.header-menu1 ul li').index($(this));
+	$('.footer-menu1 ul li').hover(function() {
+		var locate= $('.footer-menu1 ul li').index($(this));
+		$('.footer ul li').eq(locate).css('opacity','1');
 
 	}, function() {
+		var locate= $('.footer-menu1 ul li').index($(this));
+		$('.footer ul li').eq(locate).css('opacity','0');
+	});
+	$('.footer-menu ul li').hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		var locate=$('.footer-menu ul li').index($(this));
+		$('.footer-menu ul li img').eq(locate).attr({
+			'src':'images/cmn_gnavi_on_0'+(locate+1)+'.png',
+		})
+	}, function() {
 		/* Stuff to do when the mouse leaves the element */
+		var locate=$('.footer-menu ul li').index($(this));
+		$('.footer-menu ul li img').eq(locate).attr({
+			'src':'images/cmn_gnavi_0'+(locate+1)+'.png',
+		})
 	});
 });
