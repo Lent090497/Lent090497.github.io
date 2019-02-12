55f2be64ace305c6ef52fd6291f4c1daef6147ae
Nguyenthile <leevt1997@gmail.com>
2019-02-12 15:25:37 +0700

    fixed list bug

--- jquery/slider/jquery/script.js
+++ jquery/slider/jquery/script.js
@@ -3,21 +3,16 @@ $(document).ready(function() {
 	var num=0;
 	var firstImage= $('.slide img:first').attr('stt');
 	var lastImage= $('.slide img:last').attr('stt');
-	$(".slide img").each(function() {
-		if($(this).is(':visible')){
-			num= $(this).attr('stt');
-		}
-	});
+
 	//event next click
 	$("#next").click(function() {
 		num++;
 		//check change image
-		if(num>lastImage)
-		{
+		if(num>lastImage){
 			num=firstImage;
 		}
-		$('.slide img').hide();
-		$('.slide img').eq(num).show();
+		var margin_left = -648*num;
+		$('.slide-wrap').css('margin-left', margin_left +'px');
 		$('li').removeClass('active');
 		$('li').eq(num).addClass('active');
 	});
@@ -26,17 +21,15 @@ $(document).ready(function() {
 		
 		num--;
 		//check change image
-		if(num<firstImage)
-		{
+		if(num<firstImage){
 			num=lastImage;
 		}
-		$('.slide img').hide();
-		$('.slide img').eq(num).show();
+		var margin_left = -648*num;
+		$('.slide-wrap').css('margin-left', margin_left + 'px');
 		$('li').removeClass('active');
 		$('li').eq(num).addClass('active');
 	});
 	$('ul li img').click(function() {
-		/* Act on the event */
 
 		num=$(this).attr("stt")-1;
 		$('#next').click();
