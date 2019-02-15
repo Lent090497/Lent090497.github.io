feb2a253ae052c6c61dc3140bfbf8b40eb819038
Nguyenthile <leevt1997@gmail.com>
2019-02-16 00:18:30 +0700

    update index

--- responsive/ex-1/js/script.js
+++ responsive/ex-1/js/script.js
@@ -1,11 +1,74 @@
 $(document).ready(function() {
-	$('.checkE').click(function(event) {
-		/* Act on the event */
+	changeLanguage();
+	changeIconFeel();
+});
+//function change language
+function changeLanguage(){
+	$('#check1').change(function() {
+		//check checkbox Japan checked?
 		if(this.checked){
-			$('.text-blue').css("visibility","visible");
+			$('.textE').css('display','none');
 		}
 		else{
-			$('.text-blue').css("visibility","hidden");
+			$('.textE').css('display','block');
 		}
 	});
-});
+	//check checkbox English checked?
+	$('#check2').change(function() {
+		if(this.checked){
+			$('.textJ').css('display','none');
+		}
+		else{
+			$('.textJ').css('display','block');
+		}
+	});
+}
+//function change icon feel
+function changeIconFeel(){
+	//event locate1 click
+	$('#locate1').click(function() {
+		if($(this).attr('src')=='images/pic-man-smile.png'){
+			$(this).attr({
+				'src':'images/pic-man-sad.png'
+			});
+			$('#locate2').attr({
+				'src':'images/pic-man-smile.png'
+			})
+			$('.content-chat2-left').css('display','block');
+			$('.content-chat2-right').css('display','none');
+		}
+		else{
+			$(this).attr({
+				'src':'images/pic-man-smile.png'
+			});
+			$('#locate2').attr({
+				'src':'images/pic-man-sad.png'
+			})
+			$('.content-chat2-left').css('display','none');
+			$('.content-chat2-right').css('display','block');
+		}
+	});
+	//event locate2 click
+	$('#locate2').click(function(event) {
+		if($(this).attr('src')=='images/pic-man-smile.png'){
+			$(this).attr({
+				'src':'images/pic-man-sad.png'
+			});
+			$('#locate1').attr({
+				'src':'images/pic-man-smile.png'
+			})
+			$('.content-chat2-left').css('display','block');
+			$('.content-chat2-right').css('display','none');
+		}
+		else{
+			$(this).attr({
+				'src':'images/pic-man-smile.png'
+			});
+			$('#locate1').attr({
+				'src':'images/pic-man-sad.png'
+			})
+			$('.content-chat2-left').css('display','none');
+			$('.content-chat2-right').css('display','block');
+		}
+	});
+}
