97ca60095e5d178567b07d6f1d973aa97cbe9075
Nguyenthile <leevt1997@gmail.com>
2019-02-19 09:57:50 +0700

    fix fianlly

--- responsive/ex-3/js/script.js
+++ responsive/ex-3/js/script.js
@@ -1,22 +1,14 @@
 $(document).ready(function() {
-	$('.footer-menu1 ul li').hover(function() {
-		var locate= $('.footer-menu1 ul li').index($(this));
-		$('.footer ul li').eq(locate).css('opacity','1');
-
-	}, function() {
-		var locate= $('.footer-menu1 ul li').index($(this));
-		$('.footer ul li').eq(locate).css('opacity','0');
-	});
-	$('.footer-menu ul li').hover(function() {
+	$('.content-bot_menu2 ul li').hover(function() {
 		/* Stuff to do when the mouse enters the element */
-		var locate=$('.footer-menu ul li').index($(this));
-		$('.footer-menu ul li img').eq(locate).attr({
+		var locate=$('.content-bot_menu2 ul li').index($(this));
+		$('.content-bot_menu2 ul li img').eq(locate).attr({
 			'src':'images/cmn_gnavi_on_0'+(locate+1)+'.png',
 		})
 	}, function() {
 		/* Stuff to do when the mouse leaves the element */
-		var locate=$('.footer-menu ul li').index($(this));
-		$('.footer-menu ul li img').eq(locate).attr({
+		var locate=$('.content-bot_menu2 ul li').index($(this));
+		$('.content-bot_menu2 ul li img').eq(locate).attr({
 			'src':'images/cmn_gnavi_0'+(locate+1)+'.png',
 		})
 	});
