69bfa21a9265d48eaf6cd29fbedc4604f4165eeb
Nguyenthile <leevt1997@gmail.com>
2019-02-22 14:08:09 +0700

    fix time 1 ex-3 responsive

--- responsive/ex-3/js/script.js
+++ responsive/ex-3/js/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-	$('.content-bot_menu2 ul li').hover(function() {
+	$('.js-content-bot_menu2 ul li').hover(function() {
 		/* Stuff to do when the mouse enters the element */
-		var locate=$('.content-bot_menu2 ul li').index($(this));
-		$('.content-bot_menu2 ul li img').eq(locate).attr({
+		var locate=$('.js-content-bot_menu2 ul li').index($(this));
+		$('.js-content-bot_menu2 ul li img').eq(locate).attr({
 			'src':'images/cmn_gnavi_on_0'+(locate+1)+'.png',
 		})
 	}, function() {
 		/* Stuff to do when the mouse leaves the element */
-		var locate=$('.content-bot_menu2 ul li').index($(this));
-		$('.content-bot_menu2 ul li img').eq(locate).attr({
+		var locate=$('.js-content-bot_menu2 ul li').index($(this));
+		$('.js-content-bot_menu2 ul li img').eq(locate).attr({
 			'src':'images/cmn_gnavi_0'+(locate+1)+'.png',
 		})
+	});
+	$(window).resize(function(event) {
+		/* Act on the event */
+	
 	});
 });
