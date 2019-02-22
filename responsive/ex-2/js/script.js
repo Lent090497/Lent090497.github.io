aaafe661f6b70c2b305ad16462ddc76b53e5b3ad
Nguyenthile <leevt1997@gmail.com>
2019-02-22 15:17:09 +0700

    ntl

--- responsive/ex-2/js/script.js
+++ responsive/ex-2/js/script.js
@@ -4,24 +4,24 @@ $(document).ready(function () {
 
            $("html, body").stop();
            $('p').css('color','red');
-           $('.container').css('background','black');
-           $('.border').css('border-top','1px solid #330000');
-           $('.content img').eq(1).attr('src','images/banner_2.jpg');
-           $('.content img').eq(2).attr('src','images/img_5.png');
-           $('.content img').eq(4).attr('src','images/img_6.png');
-           $('.box-grey').css('background','#330000');
+           $('.js-container').css('background','black');
+           $('.js-border').css('border-top','1px solid #330000');
+           $('.js-header img').eq(2).attr('src','images/banner_2.jpg');
+           $('.js-content img').eq(0).attr('src','images/img_5.png');
+           $('.js-content img').eq(2).attr('src','images/img_6.png');
+           $('.js-box-grey').css('background','#330000');
            $("html, body").animate({scrollTop:0},5000, function () {
                setTimeout(function () {
                    $("html, body").css("overflow","auto");
                    $('p').css('color','black');
-                   $('.container').css('background','none');
-                   $('.border').css('border-top','1px solid #000000');
-                   $('.content img').eq(1).attr('src','images/TOP_02.png');
-                   $('.content img').eq(2).attr('src','images/img_1.png');
-                   $('.content img').eq(4).attr('src','images/img_3.png');
-                   $('.box-grey').css('background','#cccccc');
+                   $('.js-container').css('background','none');
+                   $('.js-border').css('border-top','1px solid #000000');
+                   $('.js-header img').eq(2).attr('src','images/TOP_02.png');
+                   $('.js-content img').eq(0).attr('src','images/img_1.png');
+                   $('.js-content img').eq(2).attr('src','images/img_3.png');
+                   $('.js-box-grey').css('background','#cccccc');
                },0);
            });
        }
-})
+  })
 });
