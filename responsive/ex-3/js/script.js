1bdaecc4b0aec6dce9fa69f96d13f71f8a69e3c9
Nguyenthile <leevt1997@gmail.com>
2019-02-22 15:47:02 +0700

    fix bug menu 2

--- responsive/ex-3/js/script.js
+++ responsive/ex-3/js/script.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 		/* Stuff to do when the mouse enters the element */
 		var locate=$('.js-content-bot_menu2 ul li').index($(this));
 		$('.js-content-bot_menu2 ul li img').eq(locate).attr({
-			'src':'images/cmn_gnavi_on_0'+(locate+1)+'.png',
+			'src':'images/image/item_content_bot_menu2_on_0'+(locate+1)+'.jpg',
 		})
 	}, function() {
 		/* Stuff to do when the mouse leaves the element */
 		var locate=$('.js-content-bot_menu2 ul li').index($(this));
 		$('.js-content-bot_menu2 ul li img').eq(locate).attr({
-			'src':'images/cmn_gnavi_0'+(locate+1)+'.png',
+			'src':'images/image/item_content_bot_menu2_0'+(locate+1)+'.jpg',
 		})
 	});
 	$(window).resize(function(event) {
