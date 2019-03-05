f27fda215320029b28bd40cf48239ca770b6a127
Nguyenthile <leevt1997@gmail.com>
2019-03-05 15:01:19 +0700

    fix bug ex-2 responsive

--- responsive/ex-2/js/script.js
+++ responsive/ex-2/js/script.js
@@ -2,7 +2,6 @@ $(window).scroll(function () {
        if($(window).scrollTop() == ($(document).height() - $(window).height())) {
           $("html, body").stop();
           $('.container').addClass('js-container');
-          $('.text-info').addClass('js-text_info');
           $('a').css('color','#990202');
           $('.border').css('border-top','1px solid #700000');
           $('.box-grey').css('background-color','#330000');
@@ -11,10 +10,10 @@ $(window).scroll(function () {
           })
           $('.content-list img').eq(0).attr('src','images/img_1_horro.png');
           $('.content-list img').eq(2).attr('src','images/img_3_horro.png');
+          $('.content-list img').eq(3).attr('src','images/img_4_horro.jpg');
           $("html, body").animate({scrollTop:0},5000, function () {
                setTimeout(function () {
                 $('.container').removeClass('js-container');
-                $('.text-info').removeClass('js-text_info');
                 $('a').css('color','#000000');
                 $('.border').css('border-top','1px solid #000000');
                 $('.box-grey').css('background-color','#cccccc');
@@ -22,7 +21,8 @@ $(window).scroll(function () {
                   'src': 'images/banner.png'
                 });
                 $('.content-list img').eq(0).attr('src','images/img_1.png');
-                $('.content-list img').eq(2).attr('src','images/img_3_horro.png');
+                $('.content-list img').eq(2).attr('src','images/img_3.png');
+                $('.content-list img').eq(3).attr('src','images/img_4.png');
                },0);
            });
        }
