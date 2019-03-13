78fc7a1130941aaa8f7eabc11f91d89834e8c8a8
Nguyenthile <leevt1997@gmail.com>
2019-03-14 00:00:20 +0700

    fix ex-6 responsive

--- responsive/ex-6/assets/js/script.js
+++ responsive/ex-6/assets/js/script.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
 	var num=0;
-	function next(){
-		num++;
-		if(num>6){
-			num=0;
+	$('.js-list ul li').click(function(event) {
+		//display procedure
+		var index=$('.js-list ul li').index($(this));
+		$('.js-list ul li').removeClass('active--list');
+		$('.js-list ul li').eq(index).addClass('active--list');
+		$('.procedure img').hide();
+		$('.procedure img').eq(index).show();
+	});
+	$('.js--list--ticket ul li').click(function(event) {
+		//display ticket
+		var index= $('.js--list--ticket ul li').index($(this));
+		if(index==0){
+			$('.js--ticket--suica').show();
+			$('.js--ticket--railpass').hide();
+			$('.js--ticket--pasmo').hide();
 		}
-		$('.js-list ul li').removeClass('active');
-		$('.js-list ul li').eq(num).addClass('active');
-	}
-	var myInterval=setInterval(function(){
-		next();
-	},5000)
+		if(index==1){
+			$('.js--ticket--suica').hide();
+			$('.js--ticket--railpass').hide();
+			$('.js--ticket--pasmo').show();
+		}
+		if(index==2){
+			$('.js--ticket--suica').hide();
+			$('.js--ticket--railpass').show();
+			$('.js--ticket--pasmo').hide();
+		}
+		$('.js--list--ticket ul li').removeClass('active--ticket')
+		$('.js--list--ticket ul li').eq(index).addClass('active--ticket');
+		$('.js--list--ticket ul li').eq(0).addClass('no-active--ticket');
+	});
 });
