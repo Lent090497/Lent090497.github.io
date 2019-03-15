c640c18aab0531ebeb0c0d659bd3af5a122399f7
Nguyenthile <leevt1997@gmail.com>
2019-03-15 17:59:21 +0700

    push ex-7 responsive

---
+++ responsive/ex-7/assets/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	var num=0;
+	var imglast1=$('.js--slide-1 img:last').index('.js--slide-1 img');
+	var myInterval1=setInterval(function(){
+		num++;
+		if(num>imglast1){
+			num=0;
+		}
+		slide1(num);
+		slide2(num);
+		slide3(num);
+	},1000)
+
+	function slide1(index){
+		$('.js--slide-1 img').hide();
+		$('.js--slide-1 img').eq(index).show();
+		$('.js--list-1 ul li img').attr({
+			'src':'assets/images/img_grey.png'
+		});		
+		$('.js--list-1 ul li img').eq(index).attr({
+			'src':'assets/images/img_black.png'
+		});
+	}
+	function slide2(index){
+		$('.js--slide-2 img').hide();
+		$('.js--slide-2 img').eq(index).show();
+		$('.js--list-2 ul li img').attr({
+			'src':'assets/images/img_grey.png'
+		});		
+		$('.js--list-2 ul li img').eq(index).attr({
+			'src':'assets/images/img_black.png'
+		});
+	}
+	function slide3(index){
+		$('.js--slide-3 img').hide();
+		$('.js--slide-3 img').eq(index).show();
+		$('.js--list-3 ul li img').attr({
+			'src':'assets/images/img_grey.png'
+		});		
+		$('.js--list-3 ul li img').eq(index).attr({
+			'src':'assets/images/img_black.png'
+		});
+	}
+});
