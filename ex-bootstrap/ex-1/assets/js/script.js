539906f56db2188a9138e6d697fd15117d59488d
Nguyenthile <leevt1997@gmail.com>
2019-04-02 17:56:36 +0700

    fix banner

--- ex-bootstrap/ex-1/assets/js/script.js
+++ ex-bootstrap/ex-1/assets/js/script.js
@@ -18,4 +18,12 @@ $(document).ready(function() {
 	  nextArrow: $('#next-logo'),
 	  prevArrow: $('#prev-logo'),
 	});
+	function zoom(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*100
+  y = offsetY/zoomer.offsetHeight*100
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
 });
