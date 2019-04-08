39549f653151f223ff4bad829cde2c341b7eb98f
Nguyenthile <leevt1997@gmail.com>
2019-04-08 15:06:55 +0700

    push ex-2 responsive

--- ex-bootstrap/ex-2/assets/js/script.js
+++ ex-bootstrap/ex-2/assets/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+});
+$(window).resize(function(event) {
+	if($(document).width()>=768){
 
-
+		$('.content-2').removeClass('flex-column-reverse');
+		$('.content-2').removeClass('flex-row');
+		$('.content-4').removeClass('flex-column-reverse');
+		$('.content-4').removeClass('flex-row');
+	}
+	else{
+		$('.content-2').addClass('flex-column-reverse');
+		$('.content-2').addClass('flex-row');
+		$('.content-4').addClass('flex-column-reverse');
+		$('.content-4').addClass('flex-row');
+	}
 });
